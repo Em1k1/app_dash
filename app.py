3daad5031ebe69dccbc19cb7ca30cc771d694f0e
Em1k1 <128514725+Em1k1@users.noreply.github.com>
2026-03-13 13:16:14 +0100

    Refactor app.py for improved readability

--- app.py
+++ app.py
@@ -115,4 +115,5 @@ app.layout = html.Div([
 # -----------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
+    app.run(host="0.0.0.0")
+
